c0d57d80a568ed703f703286b1b234d546ecc87e
Triangleaaa <2009triangle19@gmail.com>
2026-05-19 08:35:04 +0600

    fix duplicate Client error

--- index.js
+++ index.js
@@ -82,10 +82,6 @@ app.listen(process.env.PORT || 3000, () => {
 });
 const { Client, GatewayIntentBits, Events } = require("discord.js");
 
-const client = new Client({
-  intents: [GatewayIntentBits.Guilds],
-});
-
 client.once(Events.ClientReady, () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
