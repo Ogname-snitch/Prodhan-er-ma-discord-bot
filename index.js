c3e8999f4633a7d68215692b86828823c67c145c
Triangleaaa <2009triangle19@gmail.com>
2026-05-19 08:14:56 +0600

    fix issue

--- index.js
+++ index.js
@@ -70,5 +70,14 @@ client.on("voiceStateUpdate", () => {
     console.log("Rejoined VC");
   }
 });
+const express = require("express");
+const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Web server running");
+});
 client.login(process.env.TOKEN);
