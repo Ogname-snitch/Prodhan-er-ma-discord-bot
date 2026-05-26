aa05c7baaa57697bd4b6fb17a628ff2a0a32cd1a
Triangleaaa <2009triangle19@gmail.com>
2026-05-26 16:20:11 +0600

    beg nerf

--- commands/economy/beg.js
+++ commands/economy/beg.js
@@ -26,7 +26,7 @@ module.exports = {
 
     // 🟣 PERK: BEGGAR (higher jackpot chance)
     if (user.perk === "Beggar") {
-      if (Math.random() < 0.65) {
+      if (Math.random() < 0.5) {
         amount = 3000;
       }
     }
