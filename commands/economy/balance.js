6a5d686b9d7a2ccb2a94647d21f10bd225c3a6ad
Triangleaaa <2009triangle19@gmail.com>
2026-06-07 11:56:11 +0600

    balance update

--- commands/economy/balance.js
+++ commands/economy/balance.js
@@ -1,8 +1,9 @@
-const {
-  SlashCommandBuilder,
-} = require("discord.js");
-
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const User = require("../../utils/database");
+
+function format(num = 0) {
+  return num.toLocaleString(); // adds commas
+}
 
 async function getUser(id) {
   let user = await User.findOne({ userId: id });
@@ -11,10 +12,9 @@ async function getUser(id) {
     user = await User.create({
       userId: id,
       perk: "None",
-
-      // ⭐ BANK DEFAULTS
       bank: 0,
       bankSpace: 1000,
+      wallet: 0,
     });
   }
 
@@ -38,20 +38,44 @@ module.exports = {
       interaction.options.getUser("user") ||
       interaction.user;
 
-    const user =
-      await getUser(target.id);
+    const user = await getUser(target.id);
 
-    const perk =
-      user.perk || "None";
+    const wallet = format(user.wallet);
+    const bank = format(user.bank);
+    const bankSpace = format(user.bankSpace);
 
-    const bank =
-      user.bank || 0;
+    const total = format((user.wallet || 0) + (user.bank || 0));
 
-    const bankSpace =
-      user.bankSpace || 1000;
+    const embed = new EmbedBuilder()
+      .setColor(0x2b2d31)
+      .setTitle(`💰 ${target.username}'s Wallet`)
+      .setThumbnail(target.displayAvatarURL({ dynamic: true }))
+      .addFields(
+        {
+          name: "💵 Cash",
+          value: `\`${wallet}\` coins`,
+          inline: true,
+        },
+        {
+          name: "🏦 Bank",
+          value: `\`${bank} / ${bankSpace}\``,
+          inline: true,
+        },
+        {
+          name: "💎 Total Wealth",
+          value: `\`${total}\` coins`,
+          inline: false,
+        },
+        {
+          name: "⭐ Perk",
+          value: `\`${user.perk || "None"}\``,
+          inline: true,
+        }
+      )
+      .setFooter({
+        text: "Economy System • Clean UI",
+      });
 
-    return interaction.reply(
-      `💰 ${target.username} has ${user.wallet} coins\n🏦 Bank: ${bank}/${bankSpace}\n⭐ Perk: ${perk}`
-    );
+    return interaction.reply({ embeds: [embed] });
   },
 };
